8e23607ccb6d33dc23fd4c2280245d5bcc8304ee
ka-zu-ki <yamakazuki0721@gmail.com>
2021-08-29 16:17:36 +0900

    ログインしてユーザーの情報を表示するようにした

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,81 @@
-import { items } from './data'
+import { User } from './class';
+import { items } from './data';
 
-class User {
-  constructor(
-    public name: string,
-    public age: number,
-    public days: number,
-    public money: number
-  ) {}
-}
+// user
+User.signUp();
 
-const User1 = new User('Steave', 25, 0, 0);
 
+// cards
+const cards = document.getElementById('cards');
 let cardContainer = `<div>`;
-items.map((item) => {
+
+items.map((item, index) => {
   cardContainer += `
-  <div class="grid grid-cols-4 mb-1 gap-1 bg-gray-600">
-  <div class="h-20 p-1">
-  <img src=${item.imgUrl} class="h-full" />
-  </div>
-  <div class="flex flex-col justify-center col-span-2">
-  <p class="text-3xl">${item.name}</p>
-  <div class="flex mt-1">
-  <p id="itemPrice">$${item.price.toLocaleString()}</p>
-  <p id="itemPer" class="text-green-400 ml-14">+$${item.perPrice.toLocaleString()} / sec</p>
-  </div>
-  </div>
-  <div id="itemAmount" class="flex justify-center items-center text-3xl">
-  ${item.amount}
-  </div>
+  <div data-card=${index} id="card" class="grid grid-cols-4 mb-1 gap-1 bg-gray-600">
+    <div class="h-20 p-1">
+      <img src=${item.imgUrl} class="h-full" />
+    </div>
+    <div class="flex flex-col justify-center col-span-2">
+      <p class="text-3xl">${item.name}</p>
+      <div class="flex mt-1">
+        <p id="itemPrice">$${item.price.toLocaleString()}</p>
+        <p id="itemPer" class="text-green-400 ml-14">+$${item.perPrice.toLocaleString()} / sec</p>
+      </div>
+    </div>
+    <div id="itemAmount" class="flex justify-center items-center text-3xl">
+    ${item.amount}
+    </div>
   </div>
   `;
 });
+
 cardContainer += `</div>`;
 
-const cards = document.getElementById('cards');
 cards.innerHTML = cardContainer;
 
-// export default Item;
+// cardInfo
+const cardInfo = document.getElementById('cardInfo');
+const cardList = document.querySelectorAll('#card');
+
+cardList.forEach((card, i) => {
+  card.addEventListener('click', () => {
+    cards.classList.add('hidden');
+    cardInfo.classList.remove('hidden');
+
+    let cardInfoContainer = `
+    <div>
+      <div class="flex justify-between items-center">
+        <div>
+          <h2 class="text-2xl">${items[i].name}</h2>
+          <p>Max Purchases: ${items[i].max}</p>
+          <p>Price: $${items[i].price}</p>
+          <p>Get ${items[i].perPrice} extra yen per second</p>
+        </div>
+        <div class="w-2/4 h-28 flex justify-center">
+          <img
+            id="img"
+            src=${items[i].imgUrl}
+            class="h-full"
+          />
+        </div>
+      </div>
+      <p class="mt-5">How Many would you like to purchase?</p>
+      <form action="post">
+        <input type="text" class="w-full rounded">
+      </form>
+      <p class="mt-3 flex justify-end">Total: $${items[i].amount}</p>
+      <div class="grid grid-cols-2 gap-2 mt-3">
+        <div id="btnBack" class="bg-white text-blue-500 w-full h-10 flex justify-center items-center">Go Back</div>
+        <div id="btnBuy" class="bg-blue-700 text-white w-full h-10 flex justify-center items-center">Purchase</div>
+      </div>
+      </div>
+    `;
+    cardInfo.innerHTML = cardInfoContainer;
+
+    const btnBack = document.getElementById('btnBack');
+    btnBack.addEventListener('click', () => {
+      cardInfo.classList.add('hidden');
+      cards.classList.remove('hidden');
+    });
+  });
+});
